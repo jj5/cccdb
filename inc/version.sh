f02d9e3657c3ea52f810201c3188b2906d04359f
John Elliot V <jj5@jj5.net>
2024-10-21 09:07:43 +1100

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export CCCDB_NAME='CCC Database';
 export CCCDB_CODE='cccdb';
 
-export CCCDB_VERSION='0.1.3';
+export CCCDB_VERSION='0.1.5';
 export CCCDB_VERSION_MAJOR='0';
 export CCCDB_VERSION_MINOR='1';
-export CCCDB_VERSION_PATCH='3';
+export CCCDB_VERSION_PATCH='5';
 
-export CCCDB_SVN_DATE='Date: 2024-10-21 08:35:47 +1100 (Mon, 21 Oct 2024)';
-export CCCDB_SVN_REVISION='Revision: 163344936';
+export CCCDB_SVN_DATE='Date: 2024-10-21 09:07:43 +1100 (Mon, 21 Oct 2024)';
+export CCCDB_SVN_REVISION='Revision: 164171779';
 export CCCDB_SVN_AUTHOR='Author: jj5';
 
-export CCCDB_GIT_DATE='2024-10-21 08:35:47 +1100 (Mon, 21 Oct 2024)';
+export CCCDB_GIT_DATE='2024-10-21 09:07:43 +1100 (Mon, 21 Oct 2024)';
