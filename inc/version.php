53bccc45a65b58f19b0a7e72d24d64a7578c138c
John Elliot V <jj5@jj5.net>
2024-10-21 15:47:58 +1100

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'CCCDB_NAME', 'CCC Database' );
 define( 'CCCDB_CODE', 'cccdb' );
 
-//define( 'CCCDB_VERSION', '0.1.15' );
+//define( 'CCCDB_VERSION', '0.1.17' );
 define( 'CCCDB_VERSION_MAJOR', 0 );
 define( 'CCCDB_VERSION_MINOR', 1 );
-define( 'CCCDB_VERSION_PATCH', 15 );
+define( 'CCCDB_VERSION_PATCH', 17 );
 
 define(
   'CCCDB_SVN_DATE',
-  '$Date: 2024-10-21 15:42:19 +1100 (Mon, 21 Oct 2024) $'
+  '$Date: 2024-10-21 15:47:58 +1100 (Mon, 21 Oct 2024) $'
 );
-define( 'CCCDB_SVN_REVISION', '$Revision: 175715423 $' );
+define( 'CCCDB_SVN_REVISION', '$Revision: 244115514 $' );
 define( 'CCCDB_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'CCCDB_GIT_DATE', '2024-10-21 15:42:19 +1100 (Mon, 21 Oct 2024)' );
+define( 'CCCDB_GIT_DATE', '2024-10-21 15:47:58 +1100 (Mon, 21 Oct 2024)' );
